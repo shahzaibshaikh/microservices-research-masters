ab883c04e349851b3924bd56efd8d9dd71e2ef2e
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2024-03-29 18:44:07 -0400

    👌 IMPROVE:  converted user account to ts

--- user-account/src/controllers/profile.js
+++
@@ -1,9 +0,0 @@
-const profile = (req, res) => {
-  // The user information is already attached to the request by the Auth middleware
-  const user = req.user;
-
-  // You can now use the user information as needed
-  res.json({ user, message: "Signin verified successfully" });
-};
-
-module.exports = profile;
