7930371c8da9b21446ac5778875acbfa0c0b4a5c
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2024-04-15 18:05:55 -0400

    📦 NEW: added new load testing python file

---
+++ api-testing/locustfile.py
@@ -0,0 +1,50 @@
+from locust import HttpUser, task, between
+
+class TestCycle(HttpUser):
+    wait_time = between(1, 1)  # Time between consecutive requests in seconds
+    headers = {"Content-Type": "application/json", "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NWU2YmZmNjMwODM4YmYzMTI2MWY3ZWUiLCJlbWFpbCI6InRlc3RAdGVzdC5jb20iLCJpYXQiOjE3MTMyMTI1OTcsImV4cCI6MTcxMzIzNDE5N30.7pCdZGaq_KWcq_r36jLWoNo17LUOSWGYPBCUhzOQe_w"}
+    @task(3)
+    def get_orders(self):
+        self.client.get("/api/orders/")
+
+    @task(3)
+    def get_products(self):
+        self.client.get("/api/products/")
+
+    @task(2)
+    def post_signin(self):
+        body = {
+            "email": "test@test.com",
+            "password": "123456"
+        }
+        headers = {"Content-Type": "application/json"}
+        self.client.post("/api/users/signin/", json=body, headers=headers)
+
+    @task(2)
+    def get_profile(self):
+        self.client.get("/api/users/profile/",headers=self.headers)
+
+    @task(2)
+    def get_shipping(self):
+        
+        shipping_id = "661d61e19deb2a657b2cf7ef"
+        self.client.get(f"/api/shipping/{shipping_id}", headers=self.headers)
+
+    @task(1)
+    def post_orders(self):
+        body = {
+             "products":[
+                {
+                    "productId": "65e76419467cf523e9f01d7a",
+                    "quantity": 3,
+                    "amount": 7
+                },
+                {
+                    "productId": "65e7643d467cf523e9f01d7c",
+                    "quantity": 2,
+                    "amount": 3
+                }
+            ]
+        }
+        
+        self.client.post("/api/orders/", json=body, headers=self.headers)
